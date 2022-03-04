85b8dd54de8f9c8fa040ba546721a92eac2cf3d1
Prabath <prabath.ishanka1@gmail.com>
2022-03-04 12:03:07 +0530

    first commit

---
+++ Mail.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace NewEmail
+{
+    partial class Mail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblAttachments = new System.Windows.Forms.LinkLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblAttachment = new System.Windows.Forms.Label();
+            this.lblAttach = new System.Windows.Forms.Label();
+            this.txtAttach = new System.Windows.Forms.TextBox();
+            this.txtSub = new System.Windows.Forms.TextBox();
+            this.TxtBody = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(145, 359);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblAttachments
+            // 
+            this.lblAttachments.AutoSize = true;
+            this.lblAttachments.Location = new System.Drawing.Point(163, 288);
+            this.lblAttachments.Name = "lblAttachments";
+            this.lblAttachments.Size = new System.Drawing.Size(38, 13);
+            this.lblAttachments.TabIndex = 1;
+            this.lblAttachments.TabStop = true;
+            this.lblAttachments.Text = "Attach";
+            this.lblAttachments.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblAttachments_LinkClicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+            // 
+            // lblAttachment
+            // 
+            this.lblAttachment.AutoSize = true;
+            this.lblAttachment.Location = new System.Drawing.Point(52, 194);
+            this.lblAttachment.Name = "lblAttachment";
+            this.lblAttachment.Size = new System.Drawing.Size(0, 13);
+            this.lblAttachment.TabIndex = 2;
+            // 
+            // lblAttach
+            // 
+            this.lblAttach.AutoSize = true;
+            this.lblAttach.Location = new System.Drawing.Point(123, 212);
+            this.lblAttach.Name = "lblAttach";
+            this.lblAttach.Size = new System.Drawing.Size(0, 13);
+            this.lblAttach.TabIndex = 3;
+            // 
+            // txtAttach
+            // 
+            this.txtAttach.Enabled = false;
+            this.txtAttach.Location = new System.Drawing.Point(136, 250);
+            this.txtAttach.Name = "txtAttach";
+            this.txtAttach.Size = new System.Drawing.Size(100, 20);
+            this.txtAttach.TabIndex = 4;
+            // 
+            // txtSub
+            // 
+            this.txtSub.Location = new System.Drawing.Point(136, 25);
+            this.txtSub.Name = "txtSub";
+            this.txtSub.Size = new System.Drawing.Size(236, 20);
+            this.txtSub.TabIndex = 5;
+            // 
+            // TxtBody
+            // 
+            this.TxtBody.Location = new System.Drawing.Point(136, 88);
+            this.TxtBody.Multiline = true;
+            this.TxtBody.Name = "TxtBody";
+            this.TxtBody.Size = new System.Drawing.Size(236, 119);
+            this.TxtBody.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Subject";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Message";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Mail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 394);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBody);
+            this.Controls.Add(this.txtSub);
+            this.Controls.Add(this.txtAttach);
+            this.Controls.Add(this.lblAttach);
+            this.Controls.Add(this.lblAttachment);
+            this.Controls.Add(this.lblAttachments);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Mail";
+            this.Text = "Email";
+            this.Load += new System.EventHandler(this.Email_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.LinkLabel lblAttachments;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblAttachment;
+        private System.Windows.Forms.Label lblAttach;
+        private System.Windows.Forms.TextBox txtAttach;
+        private System.Windows.Forms.TextBox txtSub;
+        private System.Windows.Forms.TextBox TxtBody;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Mail.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace NewEmail
+{
+    public partial class Mail : Form
+    {
+        SAPbobsCOM.Company oCompany;
+
+        public Mail()
+        {
+            // I use SAPHANA database
+            oCompany = new SAPbobsCOM.Company();
+
+            String sqlServer = System.Configuration.ConfigurationManager.AppSettings["SS"];
+            if (sqlServer == "SQL2012") oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2012;
+            else if (sqlServer == "SQL2014") oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2014;
+            else if (sqlServer == "SQL2016") oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2016;
+            else if (sqlServer == "HANA") oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_HANADB;
+            else oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL;
+
+            oCompany.Server = ConfigurationManager.AppSettings["DS"];
+            oCompany.language = SAPbobsCOM.BoSuppLangs.ln_English;
+            oCompany.UseTrusted = Convert.ToBoolean(ConfigurationManager.AppSettings["AU"]);
+            oCompany.DbUserName = ConfigurationManager.AppSettings["UN"];
+            oCompany.DbPassword = ConfigurationManager.AppSettings["PW"];
+            oCompany.CompanyDB = ConfigurationManager.AppSettings["DB"];
+            oCompany.UserName = ConfigurationManager.AppSettings["SAPUN"];
+            oCompany.Password = ConfigurationManager.AppSettings["SAPPW"];
+
+            int con = oCompany.Connect();
+            string error = oCompany.GetLastErrorDescription();
+            //end
+            InitializeComponent();
+        }
+
+        private void Email_Load(object sender, EventArgs e)
+        {
+
+        }
+        //file attach link
+        private void lblAttachments_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+        //file name get to texbox
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            foreach (string filePath in openFileDialog1.FileNames)
+            {
+                if (File.Exists(filePath))
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    txtAttach.Text += fileName + Environment.NewLine;
+
+                }
+            }
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            SAPbobsCOM.Recordset oRecordset = (SAPbobsCOM.Recordset)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+            oRecordset.DoQuery("Select \"E_Mail\" from \"OCRD\" where \"E_Mail\" IS NOT NULL ");
+
+            string[] array = new string[oRecordset.RecordCount];
+
+            for (int x = 0; x < array.Length; x++)
+            {
+                array[x] = oRecordset.Fields.Item("E_Mail").Value;
+                string sam = array[x];
+
+                //Email function
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("perfectbss819@gmail.com");
+                message.To.Add(sam); 
+                message.Subject = txtSub.Text ;
+                message.Body = TxtBody.Text;
+
+                //attach file
+                foreach (string filePath in openFileDialog1.FileNames)
+                {
+                    if (File.Exists(filePath))
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        message.Attachments.Add(new Attachment(filePath));
+                    }
+                }
+
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);    
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential("perfectbss819@gmail.com", "Admin@1234");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+
+                try
+                {
+                    client.Send(message);                
+                }
+
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                //end Email function
+
+                oRecordset.MoveNext();
+            }
+            MessageBox.Show("OK");
+
+            }
+
+
+     
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+     
+    }
+
+    }
+
